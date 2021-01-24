b846415925c10b7a40ff6b87dc69a87b1f0e7d26
Leon Baumann <lb-191042@hs-weingarten.de>
2021-01-24 19:22:06 +0100

    Initial commit

---
+++ helpers/bookTransformer.js
@@ -0,0 +1,33 @@
+module.exports = function transform(title, description, author, coverId, subjects) {
+    let finalDesc;
+    if (typeof (description) === "object") {
+        desc = description.value;
+    }
+    else if (typeof (descripton) === "undefined") {
+        finalDesc = "No Description."
+    }
+    else {
+        finalDesc = description;
+    }
+
+    let finalAuthor;
+    if (typeof (author) === "object") {
+        finalAuthor = author.name
+    }
+    else {
+        finalAuthor = author[0].name;
+    }
+
+    let book = {
+        title: title,
+        description: finalDesc,
+        covers: {
+            L: `http://covers.openlibrary.org/b/id/${coverId}-L.jpg`,
+            M: `http://covers.openlibrary.org/b/id/${coverId}-M.jpg`,
+            S: `http://covers.openlibrary.org/b/id/${coverId}-S.jpg`,
+        },
+        author: finalAuthor,
+        subjects: subjects,
+    };
+    return book;
+}
---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const app = express();
+const axios = require("axios");
+const cors = require("cors");
+const port = process.env.PORT || 8090;
+const bookTransformer = require("./helpers/bookTransformer");
+
+//Middleware
+app.use(express.json()); //for parsing application/json
+app.use(cors()); //for configuring Cross-Origin Resource Sharing (CORS)
+
+function log(req, res, next) {
+    console.log(req.method + " Request at" + req.url);
+    next();
+}
+app.use(log);
+
+
+//Endpoints
+app.get("/subjects/:subject", function (req, res) {
+    let works;
+    let books = [];
+    console.log(req.params);
+    axios.get(`http://openlibrary.org/subjects/${req.params.subject}.json?limit=100`)
+        .then((resp) => {
+            works = resp.data.works;
+
+            return axios.all(works.map((tmp) => axios.get(`http://openlibrary.org${tmp.key}.json`)
+            ))
+        })
+        .then(axios.spread((...resp) => {
+            works.forEach((work, i) => {
+                let book = bookTransformer(work.title, resp[i].data.description, work.authors, work.cover_id, work.subject);
+                books.push(book);
+            });
+        }))
+        .then(() => {
+            res.writeHead(200, { "Content-Type": "application/json" });
+            res.end(JSON.stringify(books));
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500);
+            res.json({
+                state: false,
+                data: {},
+                errorMsg: "Oops. Something went wrong here"
+            })
+        })
+});
+
+app.listen(port, () => console.log(`Server listening on port ${port}!`));
